a430c7280a58e00fec0c8d43e97c2c29b79077e6
Cami <92045204+camidesign@users.noreply.github.com>
2021-11-26 12:38:48 +0000

    Initial Commit

---
+++ FacebookLogin/FacebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginApp.swift
+//  FacebookLogin
+//
+//  Created by Camilla on 26/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

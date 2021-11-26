d43c77309555a00f95808a88f8cec916f994719a
Cami <92045204+camidesign@users.noreply.github.com>
2021-11-26 13:24:50 +0000

    Update ContentView.swift

--- FacebookLogin/ContentView.swift
+++ FacebookLogin/ContentView.swift
@@ -24,9 +24,27 @@ struct ContentView: View {
                 
                 VStack{
                     TextField("Email", text: $email)
+                        .padding(.top, 24)
+                        .padding(.bottom, 12)
+                        .padding(.horizontal)
+                    Divider()
                     TextField("Password", text: $password)
+                        .padding(.top, 12)
+                        .padding(.bottom, 24)
+                        .padding(.horizontal)
                 }.background(Color.white)
                     .cornerRadius(4.0)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Login")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("ButtonColor"))
+                }
+
                 
             }.padding()
         }
